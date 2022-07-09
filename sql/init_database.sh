20ea77efcbffc55a472cd531fe1b29bf6ac3e622
Bruce Zhou <18911717812@189.cn>
2022-07-09 22:35:07 +0800

    add comments -- add database initializing script.

--- sql/init_database.sh
+++ sql/init_database.sh
@@ -1,71 +1,12 @@
 #!/usr/bin/bash
-sudo su
 source /etc/profile
+export MYSQL_PWD="Buptzw&812"
 echo "connect mysql!"
-mysql -hlocalhost -uroot -pBuptzw&812
-if [[ $? = 0 ]];then
-  echo "success to connect mysql."
-else
-  echo "fail to connect mysql."
-  exit 1
-fi
-
-echo "execute create_db.sql"
-mysql -e "create_db.sql"
-if [[ $? = 0 ]];then
-  echo "success to execute create_db.sql"
-else
-  echo "fail to execute create_db.sql"
-  exit 1
-fi
-
-echo "execute user.sql"
-mysql -e "user.sql"
-if [[ $? = 0 ]];then
-  echo "success to execute user.sql"
-else
-  echo "fail to execute user.sql"
-fi
-
-echo "execute video.sql"
-mysql -e "video.sql"
-if [[ $? = 0 ]];then
-  echo "success to execute video.sql"
-else
-  echo "fail to execute video.sql"
-fi
-
-echo "execute episode.sql"
-mysql -e "episode.sql"
-if [[ $? = 0 ]];then
-  echo "success to execute episode.sql"
-else
-  echo "fail to execute episode.sql"
-fi
-
-echo "execute chapter.sql"
-mysql -e "chapter.sql"
-if [[ $? = 0 ]];then
-  echo "success to execute chapter.sql"
-else
-  echo "fail to execute chapter.sql"
-fi
-
-echo "execute video_banner.sql"
-mysql -e "video_banner.sql"
-if [[ $? = 0 ]];then
-  echo "success to execute video_banner.sql"
-else
-  echo "fail to execute video_banner.sql"
-fi
-
-echo "execute video_order.sql"
-mysql -e "video_order.sql"
-if [[ $? = 0 ]];then
-  echo "success to execute video_order.sql"
-else
-  echo "fail to execute video_order.sql"
-fi
+echo "initialize database"
+mysql -hlocalhost -uroot <<EOF
+source init_db.sql;
+EOF
+echo "success to create database and tables"
 
 
 
