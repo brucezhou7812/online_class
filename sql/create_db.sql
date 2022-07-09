37db9b99d616ecad988c1478e1f69184c1e17f84
Bruce Zhou <18911717812@189.cn>
2022-07-09 14:08:36 +0800

    add comments -- add database initializing script.

---
+++ sql/create_db.sql
@@ -0,0 +1,1 @@
+create database devdb if not exists;
--- start.sh
+++ start.sh
@@ -4,17 +4,18 @@ set -o pipefail
 
 PROJECT_NAME="online_class"
 nohup java -jar ${PROJECT_NAME}.jar >${PROJECT_NAME}.log&
-start_ok = false
-if[[ $? = 0 ]];then
+start_ok=false
+if [[ $? = 0 ]];then
   sleep 3
   tail -n 10 ${PROJECT_NAME}.log
   sleep 5
   tail -n 50 ${PROJECT_NAME}.log
 fi
-aaa =  `grep "Started" ${PROJECT_NAME}.log | awk '{print $1}'`
+aaa=`grep "Started" ${PROJECT_NAME}.log | awk '{print $1}'`
 if [[ -n "${aaa}" ]]; then
   echo "app started ok"
   exit 0
 else
   echo "app started failed"
   exit 1
+fi
